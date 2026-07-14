9283c8004e6940997870f1406388cfcb8226e3e9
Masao Takaku <tmasao@acm.org>
2026-07-14 10:02:14 +0200

    add examples

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.COS_NAV_CONFIG = Object.freeze({
   endpoint: "https://dydra.com/masao/jp-cos/sparql",
   version: "v0.7.3",
-  examples: ["防災", "情報活用", "データ", "探究", "環境", "表現"],
+  examples: ["防災", "情報活用", "データ", "探究", "図書館", "環境", "表現", "態度"],
   searchPageSize: 100,
   contextInitialLimit: 25,
   contextMoreSize: 15,
